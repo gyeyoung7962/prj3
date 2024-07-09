1753200fd886d60ff9f3d625883793d5dc6f85ae
gyeyoung7962 <gaeyoung7962@naver.com>
2024-07-09 17:06:50 +0900

    commit

--- backend/mybuild1.sh
+++ backend/mybuild1.sh
@@ -1,6 +1,6 @@
 # react project build
 cd ../frontend
-yarn build
+npm run build
 
 # index.html, main.js 복사(이동) : dist -> static
 cd ../backend
@@ -13,5 +13,16 @@ mv ../frontend/dist src/main/resources/static
 ./gradlew bootJar
 
 # build image
-docker build -t arbiterkdh1/saengjoncoding .
+docker build -t gaeyoung/prj3 .
+docker push gaeyoung/prj3
+
+
+# 컨테이너 멈추고
+ssh -i src/main/resources/secret/key0527.pem ubuntu@15.164.231.30 'docker stop saengjoncoding'
+# 컨테이너 삭제
+ssh -i src/main/resources/secret/key0527.pem ubuntu@15.164.231.30 'docker rm saengjoncoding'
+# pull image
+ssh -i src/main/resources/secret/key0527.pem ubuntu@15.164.231.30 'docker pull gaeyoung/prj3'
+# 컨테이너 실행
+ssh -i src/main/resources/secret/key0527.pem ubuntu@15.164.231.30 'docker run -d -p 8080:8080 --restart always --name saengjoncoding gaeyoung/prj3'
 
